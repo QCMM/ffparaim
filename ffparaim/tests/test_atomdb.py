b790561cb1d8c6a4e3eee9546e7fed17a0bd5553
lmacaya <lmacaya@udec.cl>
2022-11-28 12:42:31 -0300

    Add conda installation, data and more tests

---
+++ ffparaim/tests/test_atomdb.py
@@ -0,0 +1,45 @@
+"""
+Unit and regression test for the ffparaim package. Testing atomdb.py.
+"""
+
+# Import package, test suite, and other packages as needed
+import os
+import pytest
+import filecmp
+import ffparaim
+
+
+from openff.toolkit.topology import Molecule
+
+from importlib_resources import files, as_file
+# from numpy.testing import assert_equal, assert_allclose
+
+
+def test_atomdb(tmpdir):
+    os.chdir(tmpdir)
+    ffp = ffparaim.FFparAIM()
+    with as_file(files('ffparaim.data').joinpath('solvent.pdb')) as pdb:
+        molecule, system_structure, system = ffp.prepare('c1ccc(cc1)O', str(pdb))
+    atomdb = ffparaim.AtomDB(molecule, ffp.method, ffp.basis)
+    assert isinstance(atomdb.molecule, Molecule) is True
+    assert atomdb.method == 'B3LYP'
+    assert atomdb.basis == 'def2-TZVP'
+
+
+def test_atomdb_invalid(tmpdir):
+    os.chdir(tmpdir)
+    ffp = ffparaim.FFparAIM()
+    pytest.raises(TypeError, ffparaim.AtomDB)
+    pytest.raises(TypeError, ffparaim.AtomDB, 'CO', ffp.method, ffp.basis)
+
+
+def test_create_table(tmpdir):
+    os.chdir(tmpdir)
+    ffp = ffparaim.FFparAIM()
+    with as_file(files('ffparaim.data').joinpath('solvent.pdb')) as pdb:
+        molecule, system_structure, system = ffp.prepare('c1ccc(cc1)O', str(pdb))
+    atomdb = ffparaim.AtomDB(molecule, ffp.method, ffp.basis)
+    rcubed_table = atomdb.create_table()
+    rcubed_table_ref = {"8": 3.2555507175615275, "1": 1.1713382554315122, "6": 5.179509879206654}
+    assert filecmp.cmp(os.path.join(tmpdir, 'rcubed_table.json'), files('ffparaim.data').joinpath('rcubed.json')) == 0
+    assert rcubed_table == rcubed_table_ref
---
+++ ffparaim/tests/test_ffderiv.py
@@ -0,0 +1,41 @@
+"""
+Unit and regression test for the ffparaim package. Testing ffderiv.py.
+"""
+
+# Import package, test suite, and other packages as needed
+import sys
+import os
+import pytest
+import filecmp
+import ffparaim
+
+from iodata import IOData
+
+from importlib_resources import files, as_file
+from numpy.testing import assert_equal
+
+
+def test_ffderiv():
+    ffd = ffparaim.ForceFieldDerivation()
+    assert ffd.data is None
+    assert ffd.grid is None
+    assert ffd.rho is None
+    assert ffd.pro_model is None
+    assert ffd.localgrids is None
+    assert ffd.results is None
+
+
+def test_ffderiv_invalid():
+    pytest.raises(TypeError, ffparaim.ForceFieldDerivation, 'test')
+
+
+def test_load_data():
+    ffd = ffparaim.ForceFieldDerivation()
+    with as_file(files('ffparaim.data').joinpath('orca_uks.molden.input')) as infile:
+        data = ffd.load_data(infile)
+    assert isinstance(data, IOData) is True
+
+
+def test_load_data_invalid():
+    ffd = ffparaim.ForceFieldDerivation()
+    pytest.raises(TypeError, ffd.load_data, 'data')
