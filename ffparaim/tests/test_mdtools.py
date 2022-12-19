b013881549128f36e14d62a687648ede97f19585
lmacaya <lmacaya@udec.cl>
2022-12-19 11:28:00 -0300

    Adding test for define_molecule function

--- ffparaim/tests/test_mdtools.py
+++ ffparaim/tests/test_mdtools.py
@@ -9,6 +9,7 @@ import pytest
 import filecmp
 import ffparaim.mdtools as mdt
 
+from openff.toolkit.topology import Molecule
 from importlib_resources import files, as_file
 from numpy.testing import assert_equal, assert_allclose
 
@@ -27,3 +28,18 @@ def test_separate_components_invalid():
     with as_file(files('ffparaim.data').joinpath('solvent.pdb')) as infile:
         pytest.raises(TypeError, mdt.separate_components, pdb_file=infile)
         pytest.raises(AttributeError, mdt.separate_components, infile, 0)
+
+
+def test_define_molecule(tmpdir):
+    os.chdir(tmpdir)
+    with as_file(files('ffparaim.data').joinpath('solvent.pdb')) as infile:
+        mdt.separate_components(infile, ':MOL')
+    mol = mdt.define_molecule('c1ccc(cc1)O')
+    assert isinstance(Molecule, mol)
+
+
+def test_define_molecule_invalid():
+    pytest.raises(TypeError, mdt.define_molecule)
+    pytest.raises(OSError, mdt.define_molecule, 'CO', 'lig.pdb')
+    with as_file(files('ffparaim.data').joinpath('lig.pdb')) as infile:
+        pytest.raises(ValueError, mdt.define_molecule, 'CO', infile)
